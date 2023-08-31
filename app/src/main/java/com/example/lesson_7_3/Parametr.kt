f35f686f3da6705d9b718ec9ff25e3ae915e4339
Aidar4ik <aidaroffical@mail.ru>
2023-08-31 17:36:00 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson_7_3/Parametr.kt
@@ -0,0 +1,9 @@
+package com.example.lesson_7_3
+
+import java.io.Serializable
+
+data class Parametr(
+    var img:Int,
+    var prof:String,
+    var name:String
+):Serializable
---
+++ app/src/main/java/com/example/lesson_7_3/RikiFragment.kt
@@ -0,0 +1,48 @@
+package com.example.lesson_7_3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.lesson_7_3.databinding.FragmentRikiBinding
+import com.example.lesson_7_3.databinding.ItemRiciBinding
+
+
+class RikiFragment : Fragment() {
+    private val RiciLIst = ArrayList<Parametr>()
+    private lateinit var binding: FragmentRikiBinding
+    private lateinit var item: ItemRiciBinding
+    private lateinit var adapterMusic: RickAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRikiBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+
+        adapterMusic = RickAdapter(RiciLIst, this::onClick)
+        binding.rici.adapter = adapterMusic
+    }
+
+    fun loadData() {
+        RiciLIst.add(Parametr(R.drawable.img, "Alive", "Rick Sanchez"))
+        RiciLIst.add(Parametr(R.drawable.img_1, "Alive", "Morty Smith"))
+        RiciLIst.add(Parametr(R.drawable.img_2, "Alive", " Albert Einstein"))
+        RiciLIst.add(Parametr(R.drawable.img_3, "Alive", " Jerry Smith"))
+    }
+
+    private fun onClick(position: Int) {
+        val bundle = Bundle()
+        bundle.putSerializable("key", RiciLIst[position])
+        findNavController().navigate(R.id.blankFragment, bundle)
+    }
+}
